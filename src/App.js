bf0ed5810036d97b62bed03f4bd0abbf6b5129c9
Prayush <prayushkale@gmail.com>
2020-06-15 19:03:06 +0530

    Added charts and search working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { InputGroup, FormControl } from 'react-bootstrap';
 import './App.css';
-import PriceChart from './chart';
 import './bootstrap.min.css';
+import Highcharts from "highcharts/highstock";
+import StockChart from "./Stock.jsx";
+
+// Load Highcharts modules
+require("highcharts/indicators/indicators")(Highcharts);
+require("highcharts/indicators/pivot-points")(Highcharts);
+require("highcharts/indicators/macd")(Highcharts);
+require("highcharts/modules/exporting")(Highcharts);
+require("highcharts/modules/map")(Highcharts);
+
 /*
 function App() {
   return (
@@ -13,11 +22,73 @@ function App() {
 }
 */
 
+// function= TIME_SERIES_INTRADAY,TIME_SERIES_DAILY,TIME_SERIES_WEEKLY,TIME_SERIES_MONTHLY
+// interval= 1min, 5min, 15min, 30min, 60min
+//
+let searchUrl = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=";
+let timeSeriesUrl = "https://www.alphavantage.co/query?function=";
+let apikey = "YE7H8OIMD1WOW1JN"; 
+
 class App extends Component {
   state = {
-      searchUrl: "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=",
-      apikey: "YE7H8OIMD1WOW1JN",
-      searchData: []
+      searchData: [],
+      symbol: "",
+      time: "TIME_SERIES_INTRADAY",
+      interval: 5,
+      time_series_data: []
+  };
+
+  stockOptions = {};
+
+  updateStockOptions = () => {
+    this.stockOptions = {
+      yAxis: [
+        {
+          height: "75%",
+          labels: {
+            align: "right",
+            x: -3,
+          },
+          title: {
+            text: this.state.symbol,
+          },
+        },
+        {
+          top: "75%",
+          height: "25%",
+          labels: {
+            align: "right",
+            x: -3,
+          },
+          offset: 0,
+          title: {
+            text: "MACD",
+          }
+        }
+      ],
+      series: [
+        {
+          data: this.state.time_series_data,
+          type: "ohlc",
+          name: this.state.symbol + " Stock Price",
+          id: this.state.symbol,
+        },
+        {
+          type: "candlestick",
+          linkedTo: this.state.symbol,
+          zIndex: 0,
+          lineWidth: 1,
+          dataLabels: {
+            overflow: "none",
+            crop: false,
+            y: 4,
+            style: {
+              fontSize: 9,
+            },
+          },
+        }
+      ],
+    }
   };
 
   sendHttpRequest = (method, url, data) => {
@@ -38,20 +109,56 @@ class App extends Component {
     });
   };
   
+  
+  handleStockClick = (e) => {
+    this.setState({symbol: e.target.innerHTML});
+    let symbol = e.target.innerHTML;
+    timeSeriesUrl += this.state.time + "&symbol=" + symbol + "&interval=" + this.state.interval + "min&apikey=" + apikey;
+    console.log(timeSeriesUrl);
+    this.sendHttpRequest('GET', timeSeriesUrl).then(responseData => {
+      // temp = responseData;
+      // this.setState({time_series_data: responseData["Time Series (5min)"], searchData: []});
+      var result = [];
+      // var keys = Object.keys(responseData["Time Series (5min)"]);
+      // keys.forEach(function(key){
+      //   result.push(responseData["Time Series (5min)"][key]);
+      // });
+      
+      // {1. open: "85.1800", 2. high: "85.4500", 3. low: "85.1500", 4. close: "85.2400", 5. volume: "405414"}
+      
+      result = Object.values(responseData["Time Series (5min)"]);
+      
+      result = result.map(i => {
+        return ([i["5. volume"]*1, i["1. open"]*1, i["2. high"]*1, i["3. low"]*1, i["4. close"]*1])
+      });
+      console.log(result);
+
+      this.setState({time_series_data: result, searchData: []});
+      this.updateStockOptions();
+      this.forceUpdate();
+    });
+    e.target.value = "";
+    document.getElementById("searchBar").value = "";
+    //setTimeout(function() {}, 100);
+    // console.log(temp);
+  }
+
   getData = (url) => {
     this.sendHttpRequest('GET', url).then(responseData => {
-      console.log(responseData);
-      console.log(responseData.bestMatches[0]["1. symbol"]);
-      this.setState({searchData: responseData.bestMatches.map(i => (<li class="list-group-item">{i["1. symbol"]}</li>))});
+        this.setState({searchData: responseData.bestMatches.map(i => {
+          return (<li class="list-group-item" onClick={this.handleStockClick }>{i["1. symbol"]}</li>);
+        })});
     })
   };
 
   handleSearchBarChange = (e) => {
       //this.setState({name: e.target.value});
-      let searchUrlComplete = this.state.searchUrl + e.target.value + "&apikey=" + this.state.apikey;
-      console.log(this.getData(searchUrlComplete));
+      let searchUrlComplete = searchUrl + e.target.value + "&apikey=" + apikey;
+      //console.log(this.getData(searchUrlComplete));
       if(e.target.value === ""){
         this.setState({ searchData: []});
+      }else {
+        this.getData(searchUrlComplete);
       }
   }
 
@@ -60,9 +167,9 @@ class App extends Component {
           <div className="app-content">
             <h1> Simple Price Chart </h1>
             
-            <div class="container">
-              <InputGroup size="lg" className="search-bar">
-                <FormControl onChange={this.handleSearchBarChange} placeholder="Enter Stock name" aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
+            <div class="container search-bar">
+              <InputGroup size="lg">
+                <FormControl id="searchBar" onChange={this.handleSearchBarChange} placeholder="Enter Stock name" aria-label="Large" aria-describedby="inputGroup-sizing-sm" />
               </InputGroup>
               <br/>
               <ul class="list-group" id="myList">
@@ -70,10 +177,8 @@ class App extends Component {
               </ul>
             </div>
               
-            <PriceChart stock="RIL" />
-            <PriceChart stock="Nifty" />
-
-              
+            <StockChart options={this.stockOptions} highcharts={Highcharts} /> 
+  
           </div>
       );
   }
--- src/chart.js
+++ src/chart.js
@@ -1,15 +1,78 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import Highcharts from "highcharts/highstock";
+import StockChart from "./Stock.jsx";
 
+// Load Highcharts modules
+require("highcharts/indicators/indicators")(Highcharts);
+require("highcharts/indicators/pivot-points")(Highcharts);
+require("highcharts/indicators/macd")(Highcharts);
+require("highcharts/modules/exporting")(Highcharts);
+require("highcharts/modules/map")(Highcharts);
+
+
+const stockOptions = {
+  yAxis: [
+    {
+      height: "75%",
+      labels: {
+        align: "right",
+        x: -3,
+      },
+      title: {
+        text: "AAPL",
+      },
+    },
+    {
+      top: "75%",
+      height: "25%",
+      labels: {
+        align: "right",
+        x: -3,
+      },
+      offset: 0,
+      title: {
+        text: "MACD",
+      },
+    },
+  ],
+  series: [
+    {
+      data: [],
+      type: "ohlc",
+      name: "AAPL Stock Price",
+      id: "aapl",
+    },
+    {
+      type: "candlestick",
+      linkedTo: "aapl",
+      zIndex: 0,
+      lineWidth: 1,
+      dataLabels: {
+        overflow: "none",
+        crop: false,
+        y: 4,
+        style: {
+          fontSize: 9,
+        },
+      },
+    },
+    {
+      type: "macd",
+      yAxis: 1,
+      linkedTo: "aapl",
+    },
+  ],
+};
 
 class chart extends Component {
-    //const { stock } = this.props;
-  
-    render() {
-      return (
-          <div className="price-chart">
-            <h3> {this.props.stock } Ticker chart </h3>
-          </div>
-      );
+  render() {
+    return (
+      <div>
+        <h2>Highstock</h2>
+        <StockChart options={stockOptions} highcharts={Highcharts} />
+      </div>
+    );
   }
 }
 
